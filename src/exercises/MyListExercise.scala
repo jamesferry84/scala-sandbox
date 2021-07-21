fb9febf66d9e3df326c45b9590f0a0752f4e6e82
James Ferry <jamesferry84@hotmail.com>
2021-07-21 20:23:59 +0100

    files added

--- src/exercises/MyListExercise.scala
+++ src/exercises/MyListExercise.scala
@@ -13,5 +13,21 @@ abstract class MyList {
      override toString => a string representation of the list
    */
 
+  abstract class MyList {
+    def head: Int
+    def tail: MyList
+    def isEmpty: Boolean
+    def add(num: Int): MyList
+  }
+
+  object EmptyList extends MyList {
+    def head: Int = ???
+    def tail: MyList = ???
+    def isEmpty = true
+    def add(num: Int) = new MyList(num, EmptyList)
+  }
+
+  class MyListImplementation extends MyList
+
 
 }
