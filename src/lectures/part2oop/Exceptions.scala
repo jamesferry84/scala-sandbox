0d59b83b8b1a4ba82853d429a2c9e60a8d1209b4
James Ferry <jamesferry84@hotmail.com>
2021-07-24 14:26:41 +0100

    added case class and object to my list

--- src/lectures/part2oop/Exceptions.scala
+++ src/lectures/part2oop/Exceptions.scala
@@ -5,5 +5,29 @@ object Exceptions extends App {
   val x: String = null
   // println(x.length) // this will crash with NULL POINTER EXCEPTION
 
-  val aWeirdValue = throw new NullPointerException
+  //Throwing Exceptions
+  //val aWeirdValue: String = throw new NullPointerException
+
+  //throwable classes extend the Throwable class
+  //Exception and Error are the major Throwable subtypes
+
+  //Catching Exception
+  def getInt(withExceptions: Boolean): Int = {
+    if(withExceptions) throw new RuntimeException("No int for you")
+    else 4200
+  }
+
+  val potentialFail = try {
+    getInt(true)
+  } catch {
+    case e: RuntimeException => println("caught a Runtime exception")
+  } finally {
+    //code that will always get executed
+    println("Finally....")
+  }
+
+  println(potentialFail)
+
+  //How to define own exceptions
+  class MyException ex
 }
